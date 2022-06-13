dc9f42af932d9b5a0b96cacd942adb07bccffa6d
mufid219 <ardianto219@gmail.com>
2022-06-13 20:01:55 +0700

    hapus class component App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// import React, { Component } from "react";
-
 import { useState, useEffect } from "react";
+
 import "./App.css";
+
 import CardList from "./components/card-list/CardList";
 import SearchBox from "./components/search-box/SearchBox";
 
@@ -42,54 +42,5 @@ const App = () => {
     </div>
   );
 };
-
-// export class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       monsters: [],
-//       searchField: "",
-//     };
-//   }
-
-//   componentDidMount() {
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//       .then((res) => res.json())
-//       .then((users) =>
-//         this.setState(() => {
-//           return { monsters: users };
-//         })
-//       );
-//   }
-
-//   handleFilter = (e) => {
-//     const searchField = e.target.value.toLocaleLowerCase();
-
-//     this.setState(() => {
-//       return { searchField };
-//     });
-//   };
-
-//   render() {
-//     const { monsters, searchField } = this.state;
-//     const { handleFilter } = this;
-
-//     const filteredMonsters = monsters.filter((monster) => {
-//       return monster.name.toLocaleLowerCase().includes(searchField);
-//     });
-
-//     return (
-//       <div>
-//         <h1 className="app-title">Monster Rolodex</h1>
-//         <SearchBox
-//           onChangeHandler={handleFilter}
-//           className="monsters-search-box"
-//           placeholder="search monsters"
-//         />
-//         <CardList monsters={filteredMonsters} />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
